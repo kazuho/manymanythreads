a87da908d4d079720f7f9d554d1b1cb3e0cf7b3f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-06 02:27:00 +0900

    added picoev_jsonrpcd

--- testjsonrpcclient.c
+++ testjsonrpcclient.c
@@ -1,4 +1,4 @@
-#define TEST_CLIENT_RES_SIZE (sizeof("{\"error\":null,\"id\":\"1\",\"result\":\"hello\"}") - 1)
+#define TEST_CLIENT_RES_SIZE (sizeof("{\"error\":null,\"id\":\"1\",\"result\":[\"hello\"]}") - 1)
 
 #define TEST_CLIENT_REQ "{\"method\":\"echo\",\"params\":[\"hello\"],\"id\":1}"
 
